bba17354ffafee215377731c03d47e7e9ce645fe
Marce Hsu <amemifta@gmail.com>
2024-04-11 13:48:31 +0800

    feat(footer): added footer with basic layout

--- src/pages/Footer/Footer.tsx
+++ src/pages/Footer/Footer.tsx
@@ -1,6 +1,15 @@
+import { Container, LogoDiv, LogoImg, SubTitle } from "./FooterStyle";
+import LogoImage from "../../assets/Footer-logo.png";
+
 const Footer = () => {
-    return (
-        <h1>Footer</h1>
-    )
-}
+  return (
+    <Container>
+      <LogoDiv>
+        <LogoImg src={LogoImage} />
+      </LogoDiv>
+
+      <SubTitle>TasteBud © 2024 All rights reserved</SubTitle>
+    </Container>
+  );
+};
 export default Footer;
---
+++ src/pages/Footer/FooterStyle.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    height: 180px;
+    background-color: #7c8c99;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+export const LogoDiv = styled.div`
+
+`
+export const LogoImg = styled.img`
+
+`
+
+export const SubTitle = styled.h2`
+
+`
