d61feb660a33b39aef6ccfde55a965f640f0d6b5
Marce Hsu <amemifta@gmail.com>
2024-04-12 16:10:49 +0800

    feat(post): added basic layout and data for post page

--- src/pages/Post/Post.tsx
+++ src/pages/Post/Post.tsx
@@ -1,7 +1,53 @@
- 
- const Post = () => {
-    return (
-        <h1>Post</h1>
-    )
- }
- export default Post;
+import { Children } from "react";
+
+const FlavourData = [
+  {
+    id: 0,
+    name: "Aroma",
+    selectedCount: 0,
+    children: [
+      {
+        id: 1,
+        name: "sap",
+        selectedCount: 0,
+        children: [
+          { id: 5, name: "FreshWood", selectedCount: 0 },
+          { id: 6, name: "WetWood", selectedCount: 0 },
+        ],
+      },
+      {
+        id: 2,
+        name: "Cedar",
+        selectedCount: 0,
+        children: [
+          { id: 7, name: "Sawdust", selectedCount: 0 },
+          { id: 8, name: "Carton", selectedCount: 0 },
+          { id: 9, name: "SharpenedPencil", selectedCount: 0 },
+        ],
+      },
+      {
+        id: 3,
+        name: "Oak",
+        selectedCount: 0,
+        children: [
+          { id: 10, name: "Resin", selectedCount: 0 },
+          { id: 11, name: "Varnish", selectedCount: 0 },
+        ],
+      },
+      {
+        id: 4,
+        name: "Pine",
+        selectedCount: 0,
+        children: [
+          { id: 12, name: "Turpentine", selectedCount: 0 },
+          { id: 13, name: "Retsina", selectedCount: 0 },
+        ],
+      },
+    ],
+  },
+];
+
+const Post = () => {
+  return <h1>Post</h1>;
+};
+export default Post;
---
+++ src/pages/Post/PostStyle.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 1140px;
+`
+export const Wrapper = styled.div`
+    width: 100%;
+    background-color: #bd9f9f
+`
+export const FlavourForm = styled.form`
+    
+`
