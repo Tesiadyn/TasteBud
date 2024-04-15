5281362cb337a3800578354d0c12bc6417f924b1
Marce Hsu <amemifta@gmail.com>
2024-04-15 15:37:04 +0800

    feat(member): added d3 sunburst graph to member page based on firestore data

--- src/pages/Member/Member.tsx
+++ src/pages/Member/Member.tsx
@@ -1,6 +1,83 @@
+import { SunburstChart } from "./SunburstChart";
+import { useEffect, useState } from "react";
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  query,
+  collection,
+  where,
+  getDocs,
+} from "firebase/firestore";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+interface TreeNode {
+  name: string;
+  value?: number;
+  children?: TreeNode[];
+}
+
 const Member = () => {
-    return <h1>Member</h1>;
-  };
-  
-  export default Member;
-  
+  const [data, setData] = useState<TreeNode | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchWheelData = async () => {
+      /* ----------------------------- firebase config ---------------------------- */
+      const firebaseConfig = {
+        apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+        authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+        projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+        appId: import.meta.env.VITE_FIREBASE_APP_ID,
+      };
+      /* ------------------------------ firebase init ----------------------------- */
+      const app = initializeApp(firebaseConfig);
+      const auth = getAuth(app);
+      const db = getFirestore();
+
+      const email = "test@test.com";
+      const password = "112233";
+
+      try {
+        const userIdent = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        const user = userIdent.user;
+        console.log("logged in as :", user.email);
+
+        const userUid = user.uid;
+
+        const q = query(
+          collection(db, "Member"),
+          where("__name__", "==", userUid)
+        );
+        const querySnapshot = await getDocs(q);
+
+        querySnapshot.docs.forEach((doc) => {
+          console.log("Document Data:", doc.data().WheelData);
+          const parsedData = JSON.parse(doc.data().WheelData);
+          console.log(parsedData);
+
+          setData(parsedData);
+          setLoading(false);
+        });
+      } catch (err: any) {
+        console.error("Login failed:", err.message);
+        setLoading(false);
+      }
+    };
+
+    fetchWheelData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return <SunburstChart data={data} />;
+};
+
+export default Member;
