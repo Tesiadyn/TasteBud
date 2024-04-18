9fde5d1586b415f4e7dab625f8a53678411c4695
Marce Hsu <amemifta@gmail.com>
2024-04-18 11:06:54 +0800

    style(header): modified header layout style

--- src/pages/Header/Header.tsx
+++ src/pages/Header/Header.tsx
@@ -10,6 +10,7 @@ import {
   SearchBar,
   SearchButton,
   SearchInput,
+  Wrapper,
 } from "./HeaderStyle";
 import LogoImage from "../../assets/Logo.png";
 import ProfileIcon from "../../assets/Profile.png";
@@ -17,31 +18,33 @@ import ProfileIcon from "../../assets/Profile.png";
 const Header = () => {
   return (
     <Container>
-      <PageLink to="/">
-        <LogoDiv>
-          <LogoImg src={LogoImage} />
-        </LogoDiv>
-      </PageLink>
-      <LinksDiv>
-        <PageLink to="/articles">
-          <LinkItem>知識專欄</LinkItem>
+      <Wrapper>
+        <PageLink to="/">
+          <LogoDiv>
+            <LogoImg src={LogoImage} />
+          </LogoDiv>
         </PageLink>
-        <PageLink to="/comments">
-          <LinkItem>酒款評鑑</LinkItem>
+        <LinksDiv>
+          <PageLink to="/articles">
+            <LinkItem>知識專欄</LinkItem>
+          </PageLink>
+          <PageLink to="/comments">
+            <LinkItem>酒款評鑑</LinkItem>
+          </PageLink>
+          <PageLink to="/events">
+            <LinkItem>品酒會</LinkItem>
+          </PageLink>
+        </LinksDiv>
+        <SearchBar>
+          <SearchInput />
+          <SearchButton />
+        </SearchBar>
+        <PageLink to="/member">
+          <ProfileDiv>
+            <ProfileImg src={ProfileIcon} />
+          </ProfileDiv>
         </PageLink>
-        <PageLink to="/events">
-          <LinkItem>品酒會</LinkItem>
-        </PageLink>
-      </LinksDiv>
-      <SearchBar>
-        <SearchInput />
-        <SearchButton />
-      </SearchBar>
-      <PageLink to="/member">
-        <ProfileDiv>
-          <ProfileImg src={ProfileIcon} />
-        </ProfileDiv>
-      </PageLink>
+      </Wrapper>
     </Container>
   );
 };
