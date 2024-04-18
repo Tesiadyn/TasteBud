6e52dd4f736c683bf5fc55deef1d23c7c03d403d
Marce Hsu <amemifta@gmail.com>
2024-04-18 12:22:06 +0800

    feat(login): added login page with basic layout

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,36 @@
+import {
+  Container,
+  Wrapper,
+  LoginSection,
+  LoginSectionTitle,
+  InputDiv,
+  LoginForm,
+  EmailInput,
+  InputLabel,
+  PasswordInput,
+  LoginButton,
+} from "./LoginStyle";
 const Login = () => {
-    return <h1>Login</h1>;
-  };
-  
-  export default Login;
-  
+  return (
+    <Container>
+      <Wrapper>
+        <LoginSection>
+          <LoginSectionTitle>Login</LoginSectionTitle>
+          <LoginForm>
+            <InputDiv>
+              <InputLabel>Email</InputLabel>
+              <EmailInput />
+            </InputDiv>
+            <InputDiv>
+              <InputLabel>Password</InputLabel>
+              <PasswordInput />
+            </InputDiv>
+            <LoginButton>註冊</LoginButton>
+          </LoginForm>
+        </LoginSection>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Login;
---
+++ src/pages/Login/LoginStyle.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+`;
+export const Wrapper = styled.div`
+  width: 1280px;
+  margin: 0 auto;
+`;
+export const LoginSection = styled.section`
+  width: 100%;
+  margin: 50px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const LoginSectionTitle = styled.h2``;
+export const InputDiv = styled.div`
+  display: flex;
+  margin: 20px 0;
+`;
+export const LoginForm = styled.form`
+  width: 30%;
+`;
+export const EmailInput = styled.input`
+  width: 80%;
+`;
+export const InputLabel = styled.label`
+  width: 20%;
+`;
+export const PasswordInput = styled.input`
+  width: 80%;
+`;
+export const LoginButton = styled.button`
+  width: 100%;
+  height: 30px;
+  text-align: center;
+`;
