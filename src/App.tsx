5c2a3abc91f6201cc77f54e4da3b9505fec4817b
Marce Hsu <amemifta@gmail.com>
2024-04-19 15:55:04 +0800

    refactor(app): changed name of comments page to products

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home.tsx";
 import Articles from "./pages/Articles/Articles.tsx";
 import Layout from "./pages/Layout/Layout.tsx";
-import Comments from "./pages/Comments/Comments.tsx";
+import Products from "./pages/Comments/Products.tsx";
 import Login from "./pages/Login/Login.tsx";
 import Member from "./pages/Member/Member.tsx";
 import Product from "./pages/Product/Product.tsx";
@@ -61,7 +61,7 @@ const App = () => {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="articles" element={<Articles />} />
-            <Route path="comments" element={<Comments />} />
+            <Route path="products" element={<Products />} />
             <Route path="events" element={<Events />} />
             <Route path="login" element={<Login />} />
             <Route path="member" element={<Member />} />
