821bb667a21dc3770f9e7948f845475bcc83f9f0
Marce Hsu <amemifta@gmail.com>
2024-04-19 16:00:21 +0800

    feat(products): products page will show product data based on firestore

---
+++ src/pages/Comments/Products.tsx
@@ -0,0 +1,65 @@
+import {
+  Container,
+  Wrapper,
+  Card,
+  CardImgDiv,
+  CardInfoDiv,
+  CardInfos,
+  CardInfoTitle,
+} from "./ProductsStyle.tsx";
+import { firestore } from "../../utilities/firebase.tsx";
+import { collection, getDocs, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
+interface ProductsData {
+  bottler: string;
+  caskType: string;
+  distillery: string;
+  picture: string;
+  size: string;
+  strength: string;
+  title: string;
+  productUid: string;
+}
+
+const Products = () => {
+  const [productsData, setProductsData] = useState<Array<ProductsData>>([]);
+
+  useEffect(() => {
+    const fetchProductsData = async () => {
+      const db = firestore;
+      try {
+        const q = query(collection(db, "Products"));
+        const querySnapShot = await getDocs(q);
+        const newData = querySnapShot.docs.map((doc) => {
+          const eventDataFromDoc = doc.data() as ProductsData;
+          return eventDataFromDoc;
+        });
+        setProductsData(newData);
+      } catch (err) {
+        console.error("Error when fetch Products data : ", err);
+      }
+    };
+    fetchProductsData();
+  }, []);
+  console.log(productsData);
+
+  return (
+    <Container>
+      <Wrapper>
+        {productsData.map((product, index) => (
+          <Card key={index} to={`/products/${product.productUid}`}>
+            <CardImgDiv $bgImage={product.picture} />
+            <CardInfoDiv>
+              <CardInfos>
+                <CardInfoTitle>{product.title}</CardInfoTitle>
+              </CardInfos>
+            </CardInfoDiv>
+          </Card>
+        ))}
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Products;
