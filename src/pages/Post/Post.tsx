62dd9dc72125d9a1fd8cffbf0caef54b3806ea85
Tesiadyn <amemifta@gmail.com>
2024-04-21 16:18:22 +0800

    refactor(post): modified method to init firebase in post page

--- src/pages/Post/Post.tsx
+++ src/pages/Post/Post.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { initializeApp } from "firebase/app";
 import {
-  getFirestore,
   query,
   collection,
   where,
@@ -9,21 +7,10 @@ import {
   doc,
   updateDoc,
 } from "firebase/firestore";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-
-/* ----------------------------- firebase config ---------------------------- */
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-};
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { firestore, auth } from "../../utilities/firebase";
 /* ------------------------------ firebase init ----------------------------- */
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore();
+const db = firestore;
 
 const email = "test@test.com";
 const password = "112233";
