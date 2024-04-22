dc8a335b6858e5308ad46b8bac22c19b3a7611ce
Marce Hsu <amemifta@gmail.com>
2024-04-22 20:18:34 +0800

    feat(event): added function to checkout current user is author or not

--- src/pages/Event/Event.tsx
+++ src/pages/Event/Event.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { firestore } from "../../utilities/firebase";
-import { query, where, getDocs, collection } from "firebase/firestore";
+import { query, where, getDocs, collection, doc, getDoc } from "firebase/firestore";
 import { useState, useEffect } from "react";
 import {
   Container,
@@ -11,6 +11,7 @@ import {
   EventTitle,
   EventText,
 } from "./EventStyle";
+import { getAuth } from "firebase/auth";
 
 interface EventData {
   coverImage: string;
@@ -27,8 +28,9 @@ interface EventData {
 const Event = () => {
   const { id } = useParams();
   const [eventData, setEventData] = useState<EventData | null>(null);
+  const [isAuthor, setIsAuthor] = useState(false);
   const db = firestore;
-
+  const auth = getAuth();
   useEffect(() => {
     const fetchEventData = async () => {
       try {
@@ -42,13 +44,31 @@ const Event = () => {
         console.error("Error when fatching product data : ", err);
       }
     };
+    const checkIsAuthor = async () => {
+      if (!auth || !auth.currentUser || !id){
+        return;
+      }
+      try {
+        const currentUserUid = auth.currentUser?.uid;
+        const eventRef = doc(firestore, "Events", id);
+        const eventDocSnapshot = await getDoc(eventRef);
+        const eventData = eventDocSnapshot.data();
+        if (eventData && eventData.organizerUid === currentUserUid) {
+          setIsAuthor(true);
+        }
+      } catch (err: any) {
+        console.error("Error when checking author : ", err.message);
+      }
+    };
     fetchEventData();
+    checkIsAuthor();
   }, []);
 
   return (
     <Container>
       <Wrapper>
         <EventSection>
+          {isAuthor ? (<h1>You are author</h1>): (<h1>You are not author</h1>)}
           <EventImgDiv>
             <EventImg src={eventData?.coverImage} />
           </EventImgDiv>
