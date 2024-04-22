4b939852c7449304487d430659153e130b64cb0a
Marce Hsu <amemifta@gmail.com>
2024-04-22 14:53:50 +0800

    refactor(signup): user's data will be added to firestore after finished signup

--- src/pages/Signup/SignUp.tsx
+++ src/pages/Signup/SignUp.tsx
@@ -10,20 +10,142 @@ import {
   InputLabel,
   InputDiv,
 } from "./SignUpStyle";
-import { auth } from "../../utilities/firebase";
+import { auth, firestore } from "../../utilities/firebase";
 import {
   createUserWithEmailAndPassword,
   onAuthStateChanged,
 } from "firebase/auth";
+import { setDoc, doc } from "firebase/firestore";
 
-const register = async (email: string, password: string) => {
-  console.log("registering");
-  try {
-    const user = await createUserWithEmailAndPassword(auth, email, password);
-    console.log("user created", user);
-  } catch (err: any) {
-    console.error("Error creating user: ", err.message);
-  }
+const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
+  console.log("handleSubmit running...");
+
+  const register = async (email: string, password: string) => {
+    try {
+      const user = await createUserWithEmailAndPassword(auth, email, password);
+      console.log("user created", user);
+      return user;
+    } catch (err: any) {
+      console.error("Error creating user: ", err.message);
+    }
+  };
+
+  const gettingEmailAndPassword = async () => {
+    try {
+      const emailInput = e.currentTarget.elements.namedItem(
+        "email"
+      ) as HTMLInputElement | null;
+      const email = emailInput?.value;
+      const passwordInput = e.currentTarget.elements.namedItem(
+        "password"
+      ) as HTMLInputElement | null;
+      const password = passwordInput?.value;
+      if (email && password) {
+        const userObj = await register(email, password);
+        if (userObj && userObj.user) {
+          const userUid = userObj?.user.uid;
+          console.log(userUid);
+          writingMemberDoc(email, userUid);
+        }
+      }
+    } catch (err: any) {
+      console.error("Error when getting email & password : ", err.message);
+    }
+  };
+
+  const writingMemberDoc = async (email: string, userUid: string) => {
+    const memberData = {
+      attendedEvents: [],
+      commentsUid: "",
+      email: email,
+      organizedEvents: [],
+      wheelData: {
+        id: 0,
+        name: "flare",
+        children: [
+          {
+            id: 1,
+            name: "Aroma",
+            children: [
+              {
+                id: 2,
+                name: "sap",
+                children: [
+                  {
+                    id: 6,
+                    name: "FreshWood",
+                    value: 0,
+                  },
+                  {
+                    id: 7,
+                    name: "WetWood",
+                    value: 0,
+                  },
+                ],
+              },
+              {
+                id: 3,
+                name: "Cedar",
+                children: [
+                  {
+                    id: 8,
+                    name: "Sawdust",
+                    value: 0,
+                  },
+                  {
+                    id: 9,
+                    name: "Carton",
+                    value: 0,
+                  },
+                  {
+                    id: 10,
+                    name: "SharpenedPencil",
+                    value: 0,
+                  },
+                ],
+              },
+              {
+                id: 4,
+                name: "Oak",
+                children: [
+                  {
+                    id: 11,
+                    name: "Resin",
+                    value: 0,
+                  },
+                  {
+                    id: 12,
+                    name: "Varnish",
+                    value: 0,
+                  },
+                ],
+              },
+              {
+                id: 5,
+                name: "Pine",
+                children: [
+                  {
+                    id: 13,
+                    name: "Turpentine",
+                    value: 0,
+                  },
+                  {
+                    id: 14,
+                    name: "Retsina",
+                    value: 0,
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    };
+    await setDoc(doc(firestore, "Members", userUid), memberData);
+  };
+
+  gettingEmailAndPassword();
 };
 
 onAuthStateChanged(auth, (user) => {
@@ -33,26 +155,6 @@ onAuthStateChanged(auth, (user) => {
     console.log("User logged out");
   }
 });
-
-const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-  event.preventDefault();
-  console.log("handleSubmit running...");
-  const emailInput = event.currentTarget.elements.namedItem(
-    "email"
-  ) as HTMLInputElement | null;
-  const email = emailInput?.value;
-  console.log(email);
-
-  const passwordInput = event.currentTarget.elements.namedItem(
-    "password"
-  ) as HTMLInputElement | null;
-  const password = passwordInput?.value;
-  console.log(password);
-
-  if (email && password) {
-    register(email, password);
-  }
-};
 
 const SignUp = () => {
   return (
