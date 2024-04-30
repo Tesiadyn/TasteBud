4717c8b2b2fe506e520bc1bb20a8cf70757a014d
Marce Hsu <tesiadyn@Marcedebijixingdiannao.local>
2024-04-30 09:20:21 +0800

    style(footer): layout footer modified

--- src/pages/Footer/FooterStyle.tsx
+++ src/pages/Footer/FooterStyle.tsx
@@ -1,20 +1,23 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 100%;
-    height: 180px;
-    background-color: #7c8c99;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
+  width: 100%;
+  height: 150px;
+  background-color: #7c8c99;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const wrapper = styled.div`
+  width: 1280px;
+`;
 export const LogoDiv = styled.div`
+    margin-top: 20px;
+`;
+export const LogoImg = styled.img``;
 
-`
-export const LogoImg = styled.img`
-
-`
-
-export const SubTitle = styled.h2`
-
-`
+export const SubTitle = styled.p`
+    color: #f7f7f7;
+    margin-top: 0;
+    font-size: 12px;
+`;
