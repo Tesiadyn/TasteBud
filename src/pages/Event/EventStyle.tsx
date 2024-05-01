fb6efead5791f3dc46b9dafdae9f6b67e31c5bac
Tesiadyn <amemifta@gmail.com>
2024-05-01 11:47:21 +0800

    style(event): modified event page to match theme color of project

--- src/pages/Event/EventStyle.tsx
+++ src/pages/Event/EventStyle.tsx
@@ -5,10 +5,14 @@ export const Container = styled.div`
 `;
 export const Wrapper = styled.div`
   width: 1280px;
+  margin: 0 auto;
 `;
 export const EventSection = styled.section`
   width: 100%;
   display: flex;
+  background-color: #e9e7e0;
+  border-radius: 4px;
+  margin: 50px 0;
 `;
 export const EventImgDiv = styled.div`
   width: 50%;
@@ -16,7 +20,11 @@ export const EventImgDiv = styled.div`
 export const EventImg = styled.img`
   width: 100%;
   height: 100%;
+  border-radius: 4px;
 `;
+export const EventInfoDiv = styled.div`
+  padding: 20px;
+`
 export const EventTitle = styled.h1``;
 export const EventText = styled.p`
   &.location {
