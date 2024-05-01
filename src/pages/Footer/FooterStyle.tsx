05fb2f83bc0730f9303e5795c9e52ee22bc1041a
Tesiadyn <amemifta@gmail.com>
2024-05-01 10:43:15 +0800

    style(footer): footer layout modified

--- src/pages/Footer/FooterStyle.tsx
+++ src/pages/Footer/FooterStyle.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   height: 150px;
-  background-color: #7c8c99;
+  background-color: #5e3106;
   display: flex;
   flex-direction: column;
   align-items: center;
