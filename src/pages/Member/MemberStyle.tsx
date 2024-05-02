d5ebcf3464918bcb1a7003fc89a5f60425cc0df4
Marce Hsu <tesiadyn@Marcedebijixingdiannao.local>
2024-05-02 16:35:25 +0800

    style(member): member page layouts modified

--- src/pages/Member/MemberStyle.tsx
+++ src/pages/Member/MemberStyle.tsx
@@ -1,6 +1,34 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-export const PageLink = styled(Link)`
-    
-`
+export const PageLink = styled(Link)``;
+export const Container = styled.div`
+  width: 100%;
+  background-color: #f7f7f7;
+`;
+export const Wrapper = styled.div`
+  height: 750px;
+  width: 1280px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  padding: 50px;
+`;
+export const WheelSection = styled.section`
+  margin: 0 auto;
+`;
+export const InfoSection = styled.section`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+`;
+export const SectionTitle = styled.h1`
+  text-align: center;
+`;
+export const InfoDiv = styled.div`
+  width: 20%;
+  display: flex;
+  flex-direction: column;
+`;
+export const InfoTitle = styled.h2``;
+export const InfoText = styled.p``;
