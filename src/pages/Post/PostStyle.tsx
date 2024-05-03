37bdaaf67bcb166359df736c7710edb30f4ac56a
Marce Hsu <tesiadyn@Marcedebijixingdiannao.local>
2024-05-03 21:26:09 +0800

    style(post): modified tree node styles

--- src/pages/Post/PostStyle.tsx
+++ src/pages/Post/PostStyle.tsx
@@ -13,6 +13,7 @@ export const CheckboxTreeWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   padding: 50px;
+  gap: 50px;
 `;
 export const CheckboxTreeDiv = styled.div`
   width: 30%;
@@ -20,9 +21,25 @@ export const CheckboxTreeDiv = styled.div`
 /* ---------------------------------- tree ---------------------------------- */
 export const TreeList = styled.ul`
   list-style: none;
+  background-color: #f7f7f7;
+  cursor: pointer;
+  box-shadow: 3px 3px 5px 2px rgba(89, 89, 89, 0.3);
+  border: 1px solid #808080;
+  padding: 10px;
+  margin-bottom: 10px;
 `;
 export const TreeItem = styled.li`
-  margin: 15px;
+  margin: 0;
+  padding: 5px 10px;
+  background-color: #ffffff;
+  border: 1px solid #e0e0e0;
+  border-radius: 4px;
+  margin-bottom: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f0f0f0;
+  }
 `;
 export const SubmitButton = styled.button`
   width: 70px;
