4d6592173af4ca9fb025515abf09fc346be630e6
Marce Hsu <amemifta@gmail.com>
2024-05-08 21:04:39 +0800

    style(signup): added min height to page container

--- src/pages/Signup/SignUpStyle.tsx
+++ src/pages/Signup/SignUpStyle.tsx
@@ -24,6 +24,7 @@ export const SignUpButton = styled.button`
 `;
 export const Container = styled.div`
   width: 100%;
+  min-height: calc(100vh - 270px);
 `;
 export const Wrapper = styled.div`
   width: 1280px;
