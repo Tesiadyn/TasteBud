f9ea27819c23b85685bc22b9a02a1fbe5851e44d
Marce Hsu <amemifta@gmail.com>
2024-05-13 20:27:07 +0800

    style(footer): added content in footer

--- src/pages/Footer/Footer.tsx
+++ src/pages/Footer/Footer.tsx
@@ -1,15 +1,50 @@
-import { Container, LogoDiv, LogoImg, SubTitle, Wrapper } from "./FooterStyle";
+import {
+  Container,
+  InfoDiv,
+  InfoDivItem,
+  LogoDiv,
+  LogoImg,
+  SubTitle,
+  Wrapper,
+  InfoDivTitle,
+  SocialDiv,
+  InfoDivWrapper,
+} from "./FooterStyle";
 import LogoImage from "../../assets/Footer-logo.png";
+import { X, Facebook, Instagram } from "iconoir-react";
 
 const Footer = () => {
   return (
     <Container>
       <Wrapper>
-      <LogoDiv>
-        <LogoImg src={LogoImage} />
-        <SubTitle>TasteBud © 2024 All rights reserved</SubTitle>
-      </LogoDiv>
-      
+        <LogoDiv>
+          <LogoImg src={LogoImage} />
+          <SubTitle>TasteBud © 2024 All rights reserved</SubTitle>
+        </LogoDiv>
+        <InfoDivWrapper>
+          <InfoDiv>
+            <InfoDivTitle>INSIDE</InfoDivTitle>
+            <InfoDivItem>Social responsibility</InfoDivItem>
+            <InfoDivItem>Stories</InfoDivItem>
+            <InfoDivItem>Podcast</InfoDivItem>
+          </InfoDiv>
+          <InfoDiv>
+            <InfoDivTitle>SUPPORT</InfoDivTitle>
+            <InfoDivItem>Service terms</InfoDivItem>
+            <InfoDivItem>Contact Us</InfoDivItem>
+          </InfoDiv>
+          <InfoDiv>
+            <InfoDivTitle>LEGAL</InfoDivTitle>
+            <InfoDivItem>Privacy policy</InfoDivItem>
+            <InfoDivItem>Cookie policy</InfoDivItem>
+            <InfoDivItem>Podcast</InfoDivItem>
+          </InfoDiv>
+        </InfoDivWrapper>
+        <SocialDiv>
+          <Facebook />
+          <X />
+          <Instagram />
+        </SocialDiv>
       </Wrapper>
     </Container>
   );
--- src/pages/Footer/FooterStyle.tsx
+++ src/pages/Footer/FooterStyle.tsx
@@ -4,21 +4,51 @@ export const Container = styled.div`
   width: 100%;
   height: 150px;
   background-color: #552100;
+  color: #e3e3e3;
 `;
 export const Wrapper = styled.div`
-  width: 100%;
+  width: 1280px;
+  margin: 0 auto;
+  height: 100%;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
+  align-items: center;
 `;
 export const LogoDiv = styled.div`
-  margin: 20px auto 0;
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 80%;
 `;
-export const LogoImg = styled.img``;
+export const LogoImg = styled.img`
+  width: 50%;
+  height: 70%;
+`;
 
 export const SubTitle = styled.p`
-  color: #f7f7f7;
-  margin-top: 0;
-  font-size: 12px;
-  text-align: center;
+  margin: 4px 0 0;
+`;
+export const InfoDiv = styled.ul`
+  padding: 0;
+`;
+export const InfoDivTitle = styled.p`
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 4px;
+  margin: 0 0 12px;
+`;
+export const InfoDivItem = styled.li`
+  list-style: none;
+  margin: 0 0 8px;
+  font-size: 14px;
+`;
+export const SocialDiv = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+export const InfoDivWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 60px;
 `;
