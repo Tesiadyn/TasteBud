74c5a209388b75ad1d06523918f50ecbed1db67e
Marce Hsu <amemifta@gmail.com>
2024-05-20 18:14:50 +0800

    test(post): added two method to test updateNodeValueInData function

---
+++ src/pages/Post/postfortest.tsx
@@ -0,0 +1,29 @@
+
+export interface TreeNode {
+  id: number;
+  name: string;
+  value: number;
+  children?: TreeNode[];
+}
+
+
+
+export const updateNodeValueInTree = (
+    nodes: TreeNode[],
+    nodeName: string,
+    incrementValue: number
+  ): TreeNode[] => {
+    return nodes.map((node) => {
+      const updatedNode = { ...node };
+      if (node.name === nodeName) {
+        updatedNode.value = (node.value || 0) + incrementValue;
+      } else if (node.children) {
+        updatedNode.children = updateNodeValueInTree(
+          node.children,
+          nodeName,
+          incrementValue
+        );
+      }
+      return updatedNode;
+    });
+  };
