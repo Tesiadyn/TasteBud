034acc2e5528b2a0924fbdac786f9b390dd1e436
Marce Hsu <amemifta@gmail.com>
2024-05-23 21:03:38 +0800

    refactor(useauth): modified method to get user state

--- src/utilities/useAuth.tsx
+++ src/utilities/useAuth.tsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
 
 export const useAuth = () => {
   const [user, setUser] = useState<any>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const auth = getAuth();
     const unsubscribe = onAuthStateChanged(auth, setUser);
     setUser(user);
     setLoading(false);
     return unsubscribe;
   }, []);
-  return {user, loading};
+  return { user, loading };
 };
